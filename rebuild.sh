dbb4815e4f0f7dbcbd14f1342f261753b89c7a80
HanHan666666 <60591430+HanHan666666@users.noreply.github.com>
2026-02-05 20:25:49 +0800

    fix: update rebuild.sh to include additional export commands and specify YAML file for arm64

--- rebuild.sh
+++ rebuild.sh
@@ -4,4 +4,8 @@
 
 
 ll-builder build
-ll-builder export
+ll-builder export 
+ll-builder export  --layer
+
+ll-builder build -f arm64/linglong.yaml  --skip-output-check
+ll-builder export -f arm64/linglong.yaml --layer 
